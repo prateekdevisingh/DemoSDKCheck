e2340b5c748e66dd6808af3deac346ea6eb0abbc
prateek31291 <prateek.chaudhary@videotap.com>
2017-07-24 17:19:21 +0530

    videotap prateek player

--- app/src/main/java/com/prateek/demosdkcheck/MainActivity.java
+++ app/src/main/java/com/prateek/demosdkcheck/MainActivity.java
@@ -3,6 +3,7 @@ package com.prateek.demosdkcheck;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.videotap.android.player.smart.SmartPlayerView;
 import com.videotap.android.player.smart.VideoTapSmartPlayer;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements VideoTapSmartPlay
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeImmersive();
         String uToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiJhYmM0OTQwNi1lZDFhLTRmYmUtYjA2NS1jY2RhMzk1ZDU5ZmEiLCJpYXQiOjE1MDA4OTA5NDE0NjMsImV4cCI6MTU2Mzk2Mjk0MTQ2M30.2wGl9yxaJZr4gFGaxN-H4lpbYI9xxWLtyoNHmSp-WSE";
 
         userAgent = Util.getUserAgent(this, getResources().getString(R.string.app_name));
@@ -29,10 +31,43 @@ public class MainActivity extends AppCompatActivity implements VideoTapSmartPlay
         videoTapSmartPlayer.prepareAndStartPlayer();
     }
 
+    private void makeImmersive() {
+        try {
+            final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hideController nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hideController status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            final int flags_portrait = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hideController status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        /*int flag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+*/
+
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                View decorView = getWindow().getDecorView();
+                decorView.setSystemUiVisibility(flags);
+            }else {
+                View decorView = getWindow().getDecorView();
+                decorView.setSystemUiVisibility(flags_portrait);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        videoTapSmartPlayer.prepareAndStartPlayer();
+        makeImmersive();
+        videoTapSmartPlayer.onConfigurationChanged(newConfig);
     }
 
     @Override
